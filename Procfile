bbab55a57eb710a81c12368a660933e0c5c8a311
sina <sina.taji0900@gmail.com>
2025-07-15 17:20:38 +0330

    test

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn bache_poosh.wsgi --bind 0.0.0.0:8000
+web: python manage.py runserver 0.0.0.0:8000
--- bache_poosh/settings.py
+++ bache_poosh/settings.py
@@ -137,17 +137,12 @@ MEDIA_URL = '/medias/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# CACHES = {
-#     'default': {
-#         'BACKEND': 'django_redis.cache.RedisCache',
-#         'LOCATION': f"redis://{config('REDIS_HOST', default='localhost')}:{config('REDIS_PORT', default='6379')}/0",
-#         'OPTIONS': {
-#             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-#         }
-#     }
-# }
 CACHES = {
     'default': {
-        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': f"redis://{config('REDIS_HOST', default='localhost')}:{config('REDIS_PORT', default='6379')}/0",
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
     }
 }
